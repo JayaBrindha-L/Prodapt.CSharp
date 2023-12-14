94b04fb72568ae78293dfa17cf227e0f6c5a5dae
Jaya Brindha L <jayabrindha.l@prodapt.com>
2023-12-14 12:20:59 +0530

    Change made

--- Prodapt.CSharp.Day5/Program.cs
+++ Prodapt.CSharp.Day5/Program.cs
@@ -4,8 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Welcome");
+            string str = "Jaya Brindha ";
+            string str1 = "   ";
+            Console.WriteLine(str.ToUpper());
+            Console.WriteLine(str.ToLower());
+            Console.WriteLine(str.Substring(5,7));
+            Console.WriteLine(str.Trim());
+            Console.WriteLine(str.Contains("Brindha"));
+            Console.WriteLine(String.IsNullOrEmpty(str1));
+            Console.WriteLine(String.IsNullOrWhiteSpace(str1));
         }
     }
 }
